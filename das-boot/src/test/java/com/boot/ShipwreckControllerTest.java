c1be7a96916e87c3d5a482ec388a002dd4d51a44
Maykel Martinez <maykelmtzd@gmail.com>
2019-01-16 12:11:50 -0500

    Uses mockito to mock the repo in order to test ShipwreckController in isolation.

---
+++ das-boot/src/test/java/com/boot/ShipwreckControllerTest.java
@@ -0,0 +1,40 @@
+package com.boot;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.boot.controller.ShipwreckController;
+import com.boot.model.Shipwreck;
+import com.boot.repository.ShipwreckRepository;
+
+public class ShipwreckControllerTest {
+
+	@InjectMocks
+	private ShipwreckController sc;
+	
+	@Mock
+	private ShipwreckRepository shipwreckRepository;
+	
+	@Before
+	public void init() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testShipwreckGet() {
+		Shipwreck sw = new Shipwreck();
+		sw.setId(1L);
+		when(shipwreckRepository.findById(1L)).thenReturn(Optional.of(sw));
+		
+		Shipwreck wreck = sc.get(1L);
+		assertEquals(1L, wreck.getId().longValue());
+	}
+}
